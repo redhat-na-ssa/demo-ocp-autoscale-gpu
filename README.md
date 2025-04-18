386252b400a4aff8f7da0bb046e425d044981cfc
Cory Latschkowski <koree@redhat.com>
2025-04-17 21:06:22 -0500

    update: config

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 Show how to autoscale GPU resources in AWS using an OpenShift MachineSet
 
 ```sh
-oc apply -k components
+until oc apply -k components; do : ; done
+```
+
+```sh
+oc -n openshift-machine-api \
+  describe cm cluster-autoscaler-status
 ```
--- components/autoscale-gpu/aws-gpu-machineset/job.sh
+++ components/autoscale-gpu/aws-gpu-machineset/job.sh
@@ -5,8 +5,26 @@
 
 INSTANCE_TYPE=${INSTANCE_TYPE:-g4dn.4xlarge}
 
+ocp_machineset_patch_accelerator(){
+  MACHINE_SET_NAME=${1:-gpu}
+  LABEL=${2:-nvidia-gpu}
+
+  oc -n openshift-machine-api \
+    patch machineset "${MACHINE_SET_NAME}" \
+    --type=merge --patch '{"spec":{"template":{"spec":{"metadata":{"labels":{"cluster-api/accelerator":"'"${LABEL}"'"}}}}}}'
+  
+  oc -n openshift-machine-api \
+    patch machineset "${MACHINE_SET_NAME}" \
+    --type=merge --patch '{"spec":{"template":{"spec":{"metadata":{"labels":{"node-role.kubernetes.io/gpu":""}}}}}}'
+}
+
 ocp_aws_cluster || exit 0
-ocp_aws_machineset_clone_worker g6.xlarge gpu-l4-machineset
-ocp_aws_machineset_clone_worker g5.xlarge gpu-a10-machineset
-ocp_aws_machineset_fix_storage gpu-l4-machineset 400
-ocp_aws_machineset_fix_storage gpu-a10-machineset 400
+
+ocp_aws_machineset_clone_worker g6.xlarge gpu-l4
+ocp_aws_machineset_clone_worker g5.xlarge gpu-a10
+
+ocp_aws_machineset_fix_storage machineset/gpu-l4 400
+ocp_aws_machineset_fix_storage machineset/gpu-a10 400
+
+ocp_machineset_patch_accelerator gpu-l4 nvidia-l4
+ocp_machineset_patch_accelerator gpu-a10 nvidia-a10
